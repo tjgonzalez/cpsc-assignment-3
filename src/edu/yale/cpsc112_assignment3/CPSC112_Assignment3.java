a0fca1f4f1fc9ee9c07e90a0412a23730978b83a
Travis Gonzalez <travisjeffrey.gonzalez@gmail.com>
2014-11-09 21:34:09 -0500

    Finished Part 1
    Got integers into one string that is a randomized inclusive list of
    numbers from 1-7

--- src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
+++ src/edu/yale/cpsc112_assignment3/CPSC112_Assignment3.java
@@ -10,14 +10,37 @@ public class CPSC112_Assignment3 {
 
   public static void main(String[] args) {
     makeMySecret();
-    isGameOver("1234");
-    isGameOver("4321");
-    isGameOver("2567");
-    isGameOver("1432");
+    //isGameOver("1234");
+    //isGameOver("4321");
+    //isGameOver("2567");
+    //isGameOver("1432");
   }
 
   public static void makeMySecret() {
      // Part 1 code goes here (please leave the next few lines at the end of the makeMySecret method)
+	  int mySecret1 = r.nextInt(7)+1;
+	  int mySecret2 = r.nextInt(7)+1;
+	  while (mySecret2 == mySecret1)
+	  {
+		   mySecret2 = r.nextInt(7)+1;
+	
+	  }
+	  int mySecret3 = r.nextInt(7)+1;
+	  while (mySecret3 == mySecret1 || mySecret3 == mySecret2)
+	  {
+		   mySecret3 = r.nextInt(7)+1;
+		 
+	  }
+	  int mySecret4 = r.nextInt(7)+1;
+	  while (mySecret4 == mySecret1 || mySecret4 == mySecret2 || mySecret4== mySecret3)
+	  {
+		   mySecret4 = r.nextInt(7)+1;
+		 
+	  }
+	  
+	  
+	  String mySecret=""+ mySecret1 + mySecret2 + mySecret3 + mySecret4 ;
+	  
 	 
     if (DEBUG)
     {
@@ -25,11 +48,11 @@ public class CPSC112_Assignment3 {
     }
   }
 
-  public static boolean isGuessValid(String input) {
+  //public static boolean isGuessValid(String input) {
     // Part 2 code goes here
-  }
+  //}
 
-  public static boolean isGameOver(String input) {
+  //public static boolean isGameOver(String input) {
     // Parts 3 and 4 code goes here
   }
-}
+//}
